ee2fee156052111b5cd7de9917db2c18b2b3facc
Pari Meena <parimeena404@gmail.com>
2025-08-14 22:47:12 +0530

    changes made in file

--- Assignment_Q/Upd_ithBit.cpp
+++ Assignment_Q/Upd_ithBit.cpp
@@ -4,7 +4,7 @@ using namespace std;
 void updateIthBit(int num, int i, int val){
     num = num & ~(1<<i); // clear the ith bit
 
-    num = num & (val<<i); // setting new value
+    num = num | (val<<i); // setting new value
     cout<<num<<endl;
 }
 
