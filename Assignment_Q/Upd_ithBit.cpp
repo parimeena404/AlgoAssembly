6c7aa7b0eabe4cdc56c119df9c3f53432fa30be5
Pari Meena <parimeena404@gmail.com>
2025-08-14 22:44:37 +0530

    new file added

---
+++ Assignment_Q/Upd_ithBit.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+void updateIthBit(int num, int i, int val){
+    num = num & ~(1<<i); // clear the ith bit
+
+    num = num & (val<<i); // setting new value
+    cout<<num<<endl;
+}
+
+int main(){
+
+    updateIthBit(7, 2, 0);
+    return 0;
+}
